43761c54546350f4c6f081e00da362da1fa634e5
Daniel <dammkami@gmail.com>
2019-12-31 11:11:49 -0300

    feat: implemented reverse proxy server and grpc server

--- controller/api.go
+++ controller/api.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"os"
 
 	"grpc-poc/controller/domain/audio"
 	"grpc-poc/rpc"
 
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 )
 
@@ -24,66 +28,63 @@ func main() {
 
 	server := grpc.NewServer()
 
-	rpc.RecordServiceServer(server, new(RecordService))
-	rpc.RecordServiceServer(server, new(RecordService))
+	rpc.RegisterRecordServiceServer(server, new(RecordService))
 
 	log.Println("Starting grpc server on port " + grpcPort)
 
 	go server.Serve(listen)
 
 	log.Println("Starting HTTP server on port " + httpPort)
-	// run()
+	run()
 }
 
-// func serveSwagger(w http.ResponseWriter, r *http.Request) {
-// 	//swagger := http.FileServer(http.Dir("./3rdparty/swagger-ui"))
-// 	fmt.Println("request", r.URL.Path)
-// 	p := strings.TrimPrefix(r.URL.Path, "/swagger/")
-// 	p = path.Join("3rdparty/swagger-ui/", p)
-// 	fmt.Println("request map ", p)
-// 	http.ServeFile(w, r, p)
-// }
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-// func run() error {
-// 	ctx := context.Background()
-// 	ctx, cancel := context.WithCancel(ctx)
-// 	defer cancel()
+	gw := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := rpc.RegisterRecordServiceHandlerFromEndpoint(ctx, gw, "localhost"+grpcPort, opts)
 
-// 	gw := runtime.NewServeMux()
-// 	opts := []grpc.DialOption{grpc.WithInsecure()}
-// 	err := rpc.RegisterDeviceServiceHandlerFromEndpoint(ctx, gw, "localhost"+grpcPort, opts)
+	if err != nil {
+		return err
+	}
 
-// 	if err != nil {
-// 		return err
-// 	}
+	mux := http.NewServeMux()
+	curdir, _ := os.Getwd()
+	fmt.Println("cur dir", curdir)
+	mux.Handle("/api/", gw)
 
-// 	mux := http.NewServeMux()
-// 	// mux.HandleFunc("/swagger/", serveSwagger)
-// 	curdir, _ := os.Getwd()
-// 	fmt.Println("cur dir", curdir)
-// 	//swagger := http.FileServer(http.Dir(filepath.Join(curdir, "3rdparty", "swagger-ui")))
-// 	//mux.Handle("/swagger/", swagger)
-// 	mux.Handle("/api/", gw)
-
-// 	return http.ListenAndServe(httpPort, mux)
-// }
+	return http.ListenAndServe(httpPort, mux)
+}
 
 type RecordService struct{}
 
 func (r *RecordService) GetRecord(ctx context.Context, id *rpc.RecordID) (*rpc.Response, error) {
-  ok, err := audio.GetRecord(id.RecordID)
-  
-	return &new(rpc.Response{
-    ok: ok,
-    error: "someMessage"
-  }), err
+	ok, err := audio.GetRecord(id.Id)
+
+	Response := new(rpc.Response)
+	Response.Ok = ok
+	Response.Record = nil
+	Error := new(rpc.Error)
+	Error.Message = err.Error()
+	Response.Error = Error
+
+	return Response, err
 }
 
 func (r *RecordService) BackupRecord(ctx context.Context, record *rpc.Record) (*rpc.Response, error) {
-  ok, err := rpc.BackupRecord(record)
-  
-  return &new(rpc.Response{
-    ok: ok,
-    error: "someMessage"
-  }), err
+	fmt.Println("Call")
+	fmt.Println(record)
+	ok, err := audio.BackupRecord(record)
+
+	Response := new(rpc.Response)
+	Response.Ok = ok
+	Response.Record = nil
+	Error := new(rpc.Error)
+	Error.Message = err.Error()
+	Response.Error = Error
+
+	return Response, err
 }
